a0ae8389e6c776b25d391b3ad8347a3c6a0e440c
Noelleeon <121403227+noelleeon@users.noreply.github.com>
2025-05-08 21:28:04 -0400

    Update urls.py

--- fbapp/urls.py
+++ fbapp/urls.py
@@ -1,16 +1,28 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.fbapp, name="fbapp"),
-    path("nav/", views.searchnav, name="searchnav"),
+    path("signup/", views.signup, name="signup"),
+    path("signin/", views.signin, name="signin"),
+    path("signout/", views.signout, name="signout"),
+    path("tankbar/", views.tankbar, name="tankbar"),
+    path("bheader/", views.bheader, name="bheader"),
     path("dash/", views.dash, name="dash"),
     path("profile/", views.profile, name="profile"),
-    path("statistics/", views.statistics, name="statistics"),
+    path("teamchoice/", views.teamchoice, name="teamchoice"),
+    path("teaminfo/", views.teaminfo, name="teaminfo"),
+    path("playerprofile/", views.playerprofile, name="playerprofile"),
+    path("playerslist/", views.playerslist, name="playerslist"),
     path("stat/", views.stat, name="stat"),
-    path("news/", views.news, name="news"),
-    path("article/", views.article, name="article"),
+    path("article/<int:articleID>", views.article, name="article"),
+    path("articles/", views.articles, name="articles"),
+    path("arthelper/", views.arthelper, name="arthelper"),
     path("makeart/", views.makeart, name="makeart"),
-    path("livechat/", views.livechat, name="livechat"),
-    path("<str:room_name>/", views.room, name="room"),
-]
+    path("deleteart/", views.deleteart, name="deleteart"),
+    path("games/", views.games, name="games"),
+    path("gameperweek/", views.gameperweek, name="gameperweek"),
+    path("playbyplay/",views.playbyplay, name="playbyplay"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
