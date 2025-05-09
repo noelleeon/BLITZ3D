dc66bc3d28207dbd2fd681f6e6ba37980f3505c4
Noelleeon <121403227+noelleeon@users.noreply.github.com>
2025-05-08 22:06:14 -0400

    Update settingsprod.py

--- footballproj/settingsprod.py
+++ footballproj/settingsprod.py
@@ -12,41 +12,58 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 from dotenv import load_dotenv
 
 load_dotenv()
 
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_URL = 'signin'
+
+AUTH_USER_MODEL = "fbapp.Member"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
+# https://medium.com/@altafkhan_24475/a-quick-guide-to-django-security-settings-84bbb255bf0a
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE= True
+CSRF_COOKIE_DOMAIN = 'blitz3d.net'
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = False
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'daphne',
     'django.contrib.staticfiles',
+    'simpy',
     'fbapp',
     'channels',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,17 +75,19 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'footballproj.urls'
 
+# https://forum.djangoproject.com/t/django-is-loading-templates-from-a-wrong-directory/1721
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "fbapp/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_processors': [
+          'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'fbapp.news.newz',
             ],
         },
     },
@@ -77,17 +96,26 @@ TEMPLATES = [
 WSGI_APPLICATION = 'footballproj.wsgi.application'
 ASGI_APPLICATION = 'footballproj.asgi.application'
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [('blitz3d.net', )],
+        },
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 # https://stackoverflow.com/questions/19189813/setting-django-up-to-use-mysql 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'USER':'doadmin',
-        'PASSWORD':os.environ.get('PASSWORD'), 
-        'HOST':os.environ.get('HOST'),
-        'PORT':os.environ.get('PORT'),
-        'NAME':'defaultdb',
+        'USER':'',
+        'PASSWORD':env(''), 
+        'HOST':env(''),
+        'PORT':env.int(''),
+        'NAME':'',
         'SSLMODE':'required',     
     }
 }
@@ -127,8 +155,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / 'fbapp' / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
